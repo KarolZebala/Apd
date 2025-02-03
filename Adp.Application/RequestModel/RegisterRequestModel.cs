bef48df15b60550d486814923dbaa23a2522deec
karolzebala <karol.d27@wp.pl>
2025-02-03 18:21:41 +0100

    Fix email validation in registration

--- Adp.Application/RequestModel/RegisterRequestModel.cs
+++ Adp.Application/RequestModel/RegisterRequestModel.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Apd.Api.RequestModels;
 
 public class RegisterRequestModel
 {
     public required string Username { get; set; }
+    [EmailAddress]
     public required string Email { get; set; }
     public required string Password { get; set; }
     public required string Role { get; set; }
